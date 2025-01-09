7624b91a95d91322093a4dad3fff51e012730753
ABIN-VARGEXE <abinvarghese2027@cs.ajce.in>
2025-01-09 08:26:03 +0530

    Update dependencies and enhance footer design
    - Upgraded Next.js and related packages to version 15.1.4 in package.json and package-lock.json.
    - Added a new logo image (Iedc-logo.png) to the public/images directory.
    - Redesigned the footer component in Footer.tsx to include the new logo and updated contact information, while removing the email subscription section for a cleaner layout.

--- src/components/landing-page/Footer.tsx
+++ src/components/landing-page/Footer.tsx
@@ -15,27 +15,16 @@ const LandingPageFooter = () => {
         {/* Top Section */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           <div className="space-y-8 md:space-y-16">
-            <h3 className='text-center md:text-left text-xl md:text-3xl font-normal'>Discover what's next. <br className='hidden md:block' /> Subscribe today.</h3>
-            <div className="space-y-2">
-              <Input
-                type="email"
-                placeholder='Enter your email'
-                className='w-full md:max-w-[432px] border-0 border-b p-0 py-4 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-none text-sm'
-                key="email-input"
-                suppressHydrationWarning={true}
-              />
-              <div className="flex items-center space-x-2 pb-5">
-                <Checkbox id="terms" className='border-gray-400 [data-state="checked"]:bg-blue-600' />
-                <Label
-                  htmlFor="terms"
-                  className="text-[10px] font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                  Yes, I have read and understood the <Link href="/" className='text-blue-600 '>privacy policy</Link> and hereby consent to receive e-mails
-                </Label>
-              </div>
-              <Button variant="outline" className='w-full md:max-w-[432px] rounded-3xl py-6 border-white bg-transparent text-white hover:bg-blue-600 hover:border-blue-600 hover:text-white'>Subscribe to our newsletter</Button>
+            <Image src="/images/Iedc-logo.png" alt="logo" width={400} height={300} className='text-sm w-full md:w-auto md:max-w-[350px] max-h-[140px]' />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+              Startups Valley Amal Jyothi TBI
+              Amal Jyothi College of Engineering Kanjirappally,
+              Koovappally P. O.
+              Kottayam District, Kerala
+              Pin Code - 686518
             </div>
           </div>
+
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
             <div className="space-y-4 flex flex-col text-xs text-center md:text-left">
               <h3 className='font-semibold text-2xl mb-4 md:mb-6'>Explore</h3>
@@ -63,17 +52,18 @@ const LandingPageFooter = () => {
 
         {/* Bottom Section */}
         <div
-          className='md:border-t pt-4 md:pt-8 flex-col-reverse gap-16 md:gap-0 md:flex-row flex items-start'
+          className='md:border-t pt-4 md:pt-8 flex justify-center w-full'
         >
-          <div className="w-full md:w-1/2">
-            <Image src="/images/BOI-logo.png" alt="logo" width={500} height={300} className='text-sm w-full md:w-auto md:max-w-[432px] max-h-[140px]' />
-          </div>
-          <div className="md:w-1/2 md:pl-6 space-y-8">
-            <h3 className='text-3xl text-center md:text-left '>The AI Innovation Consultancy</h3>
-            <div className="space-y-2">
-              <p className="block md:hidden">Privacy Statement  | Terms & Conditions | Creative Commons</p>
-              <p className='text-[9px] text-left '>197 Grand Street, Ste 5W, New York, NY 10013</p>
-            </div>
+          <div className="text-center w-full px-4">
+            <p className="text-xs sm:text-sm md:text-md break-words">
+              Copyright © {new Date().getFullYear()}
+              <a className='text-blue-600 underline hover:text-blue-400 transition-colors' 
+                 href="https://www.startupsvalley.com" 
+                 target="_blank" 
+                 rel="noopener noreferrer"
+              > Startups Valley Amal Jyothi TBI </a> 
+              All Rights Reserved
+            </p>
           </div>
         </div>
       </div>
