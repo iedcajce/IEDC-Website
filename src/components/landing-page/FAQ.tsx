efcc4b89e96ea40a62ead11f333acf58f7a40c65
aibaljose <aibaljosej@gmail.com>
2025-01-09 23:02:24 +0530

    FAQ added

--- src/components/landing-page/FAQ.tsx
+++ src/components/landing-page/FAQ.tsx
@@ -24,14 +24,14 @@ const FAQ: React.FC = () => {
   return (
     <div className="max-w-[90%] mx-auto mt-10 p-6  bg-white mb-6">
       <h2 className="text-2xl font-bold text-center mb-6">Frequently Asked Questions</h2>
-      <div className="space-y-4">
+      <div className="space-y-8">
         {faqData.map((item, index) => (
-          <div key={index} className="border rounded-lg overflow-hidden shadow-lg">
+          <div key={index} className="border rounded-lg overflow-hidden shadow-lg ">
             <div
               className="flex justify-between items-center p-4 bg-gray-100 cursor-pointer"
               onClick={() => toggleFAQ(index)}
             >
-              <span className="font-medium">{item.question}</span>
+              <span className="font-medium pl-8">{item.question}</span>
               <span className="text-xl">{activeIndex === index ? '-' : '+'}</span>
             </div>
             {activeIndex === index && (
